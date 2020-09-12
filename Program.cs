736dc082e93a1757b25afd0ce31530ae2743490a
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-12 00:04:38 -0300

    Letra Adicionada

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Letra_musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("");
+            Console.WriteLine("             ****Dias de luta, dias de gloria****");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("             ********\"Charlie Brown Jr.\"*********");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("                Canto minha vida com orgulho");
+            Console.WriteLine("                 Na minha vida tudo acontece");
+            Console.WriteLine("        Mas quanto mais a gente rala, mais a gente cresce");
+            Console.WriteLine("           Hoje estou feliz porque eu sonhei com você");
+            Console.WriteLine("           E amanhã posso chorar por não poder te ver");
+            Console.WriteLine("          Mas o seu sorriso vale mais que um diamante");
+            Console.WriteLine("           Se você vier comigo, aí nós vamos adiante");
+            Console.WriteLine("          Com a cabeça erguida e mantendo a fé em Deus");
+            Console.WriteLine("         O seu dia mais feliz vai ser o mesmo que o meu");
+            Console.WriteLine("               A vida me ensinou a nunca desistir");
+            Console.WriteLine("           Nem ganhar, nem perder mas procurar evoluir");
+            Console.WriteLine("                  Podem me tirar tudo que tenho");
+            Console.WriteLine("Só não podem me tirar as coisas boas que eu já fiz pra quem eu amo");
+            Console.WriteLine("          E eu sou feliz e canto e o universo é uma canção");
+            Console.WriteLine("                       E eu vou que vou");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                    História, nossas histórias");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                   Dias de luta, dias de glória");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                    História, nossas histórias");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                   Dias de luta, dias de glória");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                    História, nossas histórias");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                   Dias de luta, dias de glória");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                    História, nossas histórias");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                   Dias de luta, dias de glória");
+            Console.ResetColor();
+        
+
+
+
+        }
+    }
+}
